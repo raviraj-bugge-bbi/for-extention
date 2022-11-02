752642463e4bc4248c2f4436e1db7273b5169c3a
raviraj-bugge-bbi <raviraj.bugge@media4.com>
2022-11-02 16:50:49 +0530

    html page

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
+
 <head>
 <title>Text File</title>
 <script type="text/javascript" src="api.json"></script>
 </head>
 <body>
     <textarea name="apiData" id="apiData" cols="30" rows="10"></textarea>
-    <script>
-
-    </script>
 </body>
